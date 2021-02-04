0af0514d869f7c62bb524829ddea0ba384d09b2e
arlbibek <aryalbmail@gmail.com>
2021-02-04 19:53:49 +0545

    added comand line arguments functipnality

--- rename_files.py
+++ rename_files.py
@@ -1,22 +1,26 @@
 # Rename all file on a folder to randomly generated 6 digit code.
-
 from os import listdir, rename
-from os.path import isfile, join, exists
+from os.path import isfile, join, exists, basename, abspath
+from sys import argv
+print(f"""
+FILE: {basename(__file__)}
 
-print(f"""{__file__}
 Rename all file on a folder to randomly generated 6 digit code.
-    Eg: __fjdsl7439fRHzzt77UUvfTRe7362.png ==> 544305.png
-""")
+    Eg: __fjdsl7439fRHzzt77UUvfTRe7362.png ==> 544305.png""")
+
+# Aruments if any
+dirpath = argv[1].strip().strip('"').strip("'") if len(argv) > 1 and not isfile(argv[1]) and exists(argv[1]) else ''
 
 
-def get_name_ext(full_name):
+def get_name_ext(fpath):
     '''Returns name, extension from a file name'''
-    split = full_name.split('.')
-    if len(split) < 2: return full_name, None
+    fname = basename(fpath)
+    split = fname.split('.')
+    if len(split) < 2: return fname, None
     ext = split[-1]
     del split[-1]
-    name = '.'.join([i for i in split])
-    return name, ext
+    fname = '.'.join([i for i in split])
+    return fname, ext
 
 
 def getrand6():
@@ -25,76 +29,78 @@ def getrand6():
     return str(random.randrange(100000, 1000000))
 
 
-while True:
+def getdirpath():
+    '''Get path of a valid directory'''
     while True:
         try:
-            mypath = input('Enter directory (path)\n: ').strip()
-            if len(mypath) < 1: continue
+            path = input('\nEnter directory (path)\n: ').strip()
+            if len(path) < 1: continue
+            if isfile(path):
+                print(f"\n'{path}' is a file not a directory.")
+                continue
+            if exists(path) is False:
+                print(f"\nPath '{path}' does not exists.")
+                continue
+            return abspath(path)
         except KeyboardInterrupt: exit('Abort!')
-        if isfile(mypath):
-            print('\nIs it a file. We need directory (path).')
-            continue
-        if exists(mypath) is False:
-            print(f"\nPath '{mypath} does not exists.'\nTry again!")
-            continue
-        break
-    # Listing the contains
-    files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    if len(files) > 0: break
-    else: print("The directory doesn't seem to have any files.\nTry again with different directory (path)\n")
 
-# Setting file count limit
-if len(files) > 900_000:
-    print(
-        "Sorry! :(\nI'm not designed to rename more than 900,000 files at a time.")
-    exit(f"The directory has {len(files)}.\nBye..")
 
-print(f"{len(files)} file(s) found on '{mypath}'")
-# print([f for f in files])  # print all file name as a as a list
-print()
-print('Note: File matching the following patterns will not be affected.')
-print(' - Files with the name length less then seven excluding file extension\n   (eg. boo.mp3, 348233.png)')
-print(' - Files without extension')
-print(" - Files with '.py' extension")
-print(" - Files with '.ini' extension")
-print(" - Files that starts with '.' extension (hidden files)")
+def info():
+    '''Prints addtional information'''
+    print("""
+# Addtional info.
+> File matching the following patterns will not be affected.
+- Files with the name length less then seven excluding file extension\n   (eg. boo.mp3, 348233.png)
+- Files without extension
+- Files with '.py' extension
+- Files with '.ini' extension
+- Files that starts with '.' extension (hidden files)""")
 
+
+if not len(dirpath) < 1: dirpath = abspath(dirpath)
+else: dirpath = getdirpath()
+
+# all files on the provided directory path
+files = [f for f in listdir(dirpath) if isfile(join(dirpath, f))]
+
+# Addtional info.
+info()
+
+print(f'''
+# Directory Info.
+  Path  : {dirpath}
+  Name  : {basename(dirpath)}
+  Items : {len(files)}  # Not all files are affected, see Addtional Info above.''')
 
 # Final confirmation
-try: input("\nPress [RETURN] to continue or [CTRL + C] to abort..\n")
-except KeyboardInterrupt: exit('Abort!')
+try: input("\nPress [RETURN] to continue or [CTRL + C] to abort..")
+except KeyboardInterrupt: exit('\nAbort!')
 
 count = 0
 for file in files:
-    nameandext = get_name_ext(file)
-    name = nameandext[0]
-    ext = nameandext[-1]
+    name, ext = get_name_ext(abspath(file))
 
     # Skips
-    if file.endswith('.py'): continue  # python files
-    if file.endswith('.ini'): continue
-    if file.startswith('.'): continue
-    if ext is None: continue  # files without extension
-    # files with name less than six letters (or numbers)
-    if len(name) < 7: continue
+    if file.endswith('.py'): continue   # Python files
+    if file.endswith('.ini'): continue  # Init files
+    if file.startswith('.'): continue   # Hidden files
+    if ext is None: continue            # Files without extension
+    if len(name) < 7: continue          # Files with name of 6 (or less) characters
 
     # Renaming files
     while True:
-        src = f'{mypath}\\{file}'
+        src = join(dirpath, file)
         newname = f'{getrand6()}.{ext}'
-        dst = f'{mypath}\\{newname}'
-        if exists(dst) is True: print(
-            f'File "{newname}" already exists.Trying again!.')
-        else: break
+        dst = join(dirpath, newname)
+        if not exists(dst): break
     count += 1
-    print(f'{newname} <== {file}')
+    print(f"{newname} <== {file}")
     rename(src, dst)
+
 msg = 'Noting to rename.'
 if count != 0:
     print('^New Name      ^Original Name')
     msg = f'{count} files renamed.'
 
-print(f"\n[ Done ] {msg}\nBye..")
-input('Enter [RETURN] to exit..')
-exit()
+print(f"\n[ Done ] {msg}")
 # The End.
