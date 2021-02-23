8419134802e148b7f987a0825ba49e0eb7408dc5
arlbibek <aryalbmail@gmail.com>
2021-02-23 18:07:02 +0545

    Renamed from create_file.py to createfile.py

--- createfile.py
+++ createfile.py
@@ -1,14 +1,15 @@
 def createfile(gb):
     gb *= 1024
-    with open(f'{str(gb/1024)}GiB', "w") as fh:
+    with open(f'{str(gb/1024)}GB', "w") as fh:
             fh.seek((1024 * 1024 * gb) - 1)
             fh.write('\0')
 
 def getsize():
     while True:
-        try: return int(input('Enter file size in GiB: '))
+        try: return int(input('Enter file size in GB: '))
         except ValueError: print('\nEnter an interger value.')
         except KeyboardInterrupt: exit('Abort!')
 
-print('CAUTION! '*3)
-createfile(getsize())
+if __name__ == "--main--":
+    print('CAUTION! '*3)
+    createfile(getsize())
