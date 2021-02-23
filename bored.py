c7af4937c60a2f8de8ad16d113f3a8f66811ac49
arlbibek <aryalbmail@gmail.com>
2021-02-23 19:21:51 +0545

    enjoy ;)

--- bored.py
+++ bored.py
@@ -1,3 +1,9 @@
-# Hi, this is bored.py
-print("Hi I'm bored.")
-input()
+def main():
+    from webbrowser import open
+    print("Hi I'm bored to.")
+    open('https://youtu.be/dQw4w9WgXcQ')
+    try: input()
+    except: pass
+
+if __name__ == "__main__":
+    main()
