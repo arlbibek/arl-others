49db5a6b41aad29b2e6b694597a190b5bfa593f6
arlbibek <aryalbmail@gmail.com>
2021-02-24 19:40:21 +0545

    Fixed a traceback

--- getyesno.py
+++ getyesno.py
@@ -1,21 +1,17 @@
 def getyesno(question='Continue'):
     """Returns 'True' if input is 'yes', 'False' if 'no'"""
-    print()
     if not question.endswith('?'): question += '?'
     while True:
         try:
             ans = input(f'{question} [Y/n] ').lower().strip()
             if ans in ['y', 'yes']: return True
             elif ans in ['n', 'no']: return False
-        except: print()
-
+        except: pass
 
 if __name__ == "__main__":
-    print(f"""{__file__}
-    '''a simple yes or no question'''
-    """)
-    print('Have a nice day!' if ynq(
-        "Is the sky blue?") else 'Grass is always greener on the other side man.')
-    exit('Bye..')
+    print('Have a nice day!' if getyesno(
+        "Is the sky blue?") else 'Grass is always greener on the other side.')
+    input('Bye..')
+    exit()
 
     # The End.
