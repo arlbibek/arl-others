9d1da85064b1c2841a16780d3961d7629bdfe398
arlbibek <aryalbmail@gmail.com>
2021-02-24 20:37:51 +0545

    now displays proper msg for none windows systems

--- refreshWin.py
+++ refreshWin.py
@@ -4,10 +4,13 @@
 def refreshWin(n):
     '''Refresh windows n numbers of times
     Requrement: pyautogui'''
-    from pyautogui import hotkey, rightClick, click
-    from platform import system as pfsys
 
-    if pfsys().lower().startswith('windows'):
+    # Identifying platform and working accordingly
+    from platform import system
+    if not system().lower().startswith('win'):
+        print("[ Aborted! ] Couldn't refresh on a none Windows system.")
+    else:
+        from pyautogui import hotkey, rightClick, click
         if not n < 1:
             hotkey('win', 'd')
             for i in range(n):
@@ -15,23 +18,18 @@ def refreshWin(n):
                 click(x=1472, y=253)
             hotkey('win', 'd')
             print('Refreshed', i + 1, 'time(s).')
-    else: print("Couldn't refresh on a none Windows system.")
 
 
-if __name__ == '__main__':
+def main():
     from sys import argv
     from os.path import basename
-    print(f"""Name: {basename(__file__)}
-
-'''Refresh windws 'n' numbers of times.'''
-
-Requirements: pyautogui
-""")
-
     try:
         if len(argv) > 1: refreshWin(int(argv[1].strip().strip('-')))
-        else: refreshWin(int(input('How many times do your want refresh windows?\n:')))
+        else: refreshWin(int(input('How many times do your want refresh windows?\n: ')))
     except Exception as e: print(e)
     except KeyboardInterrupt: exit('Abort!')
+
+
+if __name__ == '__main__': main()
 
 # The End.
