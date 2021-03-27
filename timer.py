9cd6a574720805141630005ccad2e8b49bd91587
arlbibek <aryalbmail@gmail.com>
2021-03-27 19:52:02 +0545

    Added argument support

--- timer.py
+++ timer.py
@@ -1,30 +1,36 @@
-def timer(time, msg='Please wait'):
-    '''Prints like clock ticking
-    Eg. {msg} {time} sec..'''
+def timer(time, msg=''):
+    '''Prints like clock ticking'''
     from time import sleep
-    Tcount = time
+    time_count = time
     try:
-        fill = len(str(Tcount))
-        for i in range(1, time + 1):
+        fill = len(str(time_count))
+        for _ in range(1, time + 1):
             print(
-                f"{msg} {str(Tcount).zfill(fill)}/{str(time).zfill(fill)} sec..", end='\r')
+                f"{msg} {str(time_count).zfill(fill)}/{str(time).zfill(fill)} sec..", end='\r')
             sleep(1)
-            Tcount -= 1
-        print(f"{msg} {Tcount}/{time} sec..", end='\r')
+            time_count -= 1
+        print(f"{msg} {time_count}/{time} sec..", end='\r')
         print()
-    except KeyboardInterrupt: exit('\nAbort!')
+    except KeyboardInterrupt as e:
+        print(e)
 
 
 if __name__ == '__main__':
-    from os.path import basename as bn
-    print(f'''{bn(__file__)}
-"""Prints like clock ticking"""
-''')
-    try:
-        timer(int(input('Insert time in seconds.\n: ')), 'Timer:')
-        print('[ Done ] Bye..')
-    except Exception as e: exit(e)
-    except KeyboardInterrupt: exit('Abort!')
-
-# Pausing the porgram at the end
-if __name__ == '__main__': input()
+    from sys import argv
+    print(f"\n\t'''Prints like clock ticking'''\n")
+    while True:
+        if len(argv) > 1:
+            try:
+                timer(int(argv[-1]), 'Timer:')
+                print('[ Done ] ')
+                break
+            except ValueError as e:
+                pass
+        try:
+            timer(int(input('Insert time in seconds\n: ')), 'Timer:')
+            print('[ Done ] ')
+            break
+        except ValueError as e:
+            break
+        except KeyboardInterrupt:
+            break
