4666bb502ffb64e3269101ecfc215da270098004
Bibek <53701945+arlbibek@users.noreply.github.com>
2022-06-09 15:47:22 +0545

    Updated get_yes_no.py

--- get_yes_no.py
+++ get_yes_no.py
@@ -1,17 +1,24 @@
-def getyesno(question='Continue'):
-    """Returns 'True' if input is 'yes', 'False' if 'no'"""
-    if not question.endswith('?'): question += '?'
+def get_yes_no(question='Continue', error=False):
+    """Returns 'True' if input is 'yes', 'False' if 'no'
+    Set error=True to return 'None' on Keyboard interruption, other wise ask till you get either 'Yes' or 'No'"""
+    if not question.endswith('?'):
+        question += '?'
     while True:
         try:
             ans = input(f'{question} [Y/n] ').lower().strip()
-            if ans in ['y', 'yes']: return True
-            elif ans in ['n', 'no']: return False
-        except: pass
+            if ans in ['y', 'yes']:
+                return True
+            elif ans in ['n', 'no']:
+                return False
+        except KeyboardInterrupt:
+            if error:
+                print("[ Keyboard INterrupt ]")
+                return None
+            else:
+                print()
+                continue
+
 
 if __name__ == "__main__":
-    print('Have a nice day!' if getyesno(
-        "Is the sky blue?") else 'Grass is always greener on the other side.')
-    input('Bye..')
-    exit()
-
-    # The End.
+    print('Have a nice day!' if get_yes_no(
+        "Is the sky blue?", False) else 'Grass is always greener on the other side.')
