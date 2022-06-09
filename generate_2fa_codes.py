afbd65515e903a8e30571f1da135d926be07fede
Bibek <53701945+arlbibek@users.noreply.github.com>
2022-06-09 15:38:15 +0545

    Updated generate_2fa_codes.py

--- generate_2fa_codes.py
+++ generate_2fa_codes.py
@@ -1,24 +1,19 @@
-print(f"""{__file__}
 
-'''Generate 6 digit Two Factor authentication Codes'''
-
-The generated codes will be written into a text file.
-""")
-
-
-def generate(fname):
-    """writes 6 digit Two Factor authentication codes to 'fname'"""
-    with open(fname, 'wt') as fa:
-        print('Just a sec..')
+def generate(filename="2FA_code.txt", display=False):
+    """Writes 6 digit Two Factor authentication codes to 'filename'
+    Set  display=True to print the codes as well."""
+    with open(filename, 'wt') as fa:
         for n in range(100000, 1000000):
             try:
                 fa.write(str(n) + '\n')
+                if display:
+                    print(n)
             except KeyboardInterrupt:
-                continue
+                print("[ Keyboard Interrupt ]")
+                return None
+        print(
+            f"[ Done ] Results has been written to the text file named '{filename}'.")
 
 
-filename = '2FA_code.txt'
-generate(filename)
-
-exit(f"[ Done ] Results has been written to the text file named '{filename}'.")
-# The End.
+if __name__ == "__main__":
+    generate(display=True)
